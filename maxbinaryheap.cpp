fe19b235dd4f31c9c81c8f7f737a3c2f66cdc0ed
Sincere-Dhairya <varshneydhairya4@gmail.com>
2021-12-01 16:32:27 +0530

    1dec2021

--- maxbinaryheap.cpp
+++ maxbinaryheap.cpp
@@ -4,52 +4,50 @@
 using namespace std;
 
 class MaxHeap{
-    int capacity, heapsize;
-    vector<int> arr;
     public:
-    MaxHeap(int val){
-        this->capacity = val, heapsize = 0;
-        arr=vector<int>(capacity, INT_MIN);
+    int capacity, size;
+    vector<int> arr;
+    MaxHeap(int num){
+        capacity = num, size= 0, arr = vector<int> (num);
     }
-    int getparent(int i ){ return (i-1)/2; }
-    int getleft(int i) { return 2*i +1; }
-    int getright(int i) { return 2*i + 2; }
-    void insertion(int val){
-        if(heapsize == capacity){
-            cout<<"Overflow!"<<endl;
-            return;
-        }
-        ++heapsize;
-        arr[heapsize-1] = val;
-        int i = heapsize-1, par = getparent(i);
-        while(i > 0 && arr[par] < arr[i]){
-            swap(arr[i], arr[par]);
-            i = par;
+    int getparent(int i) { return (i-1)/2; }
+    int ileft (int i) { return 2*i+1; }
+    int iright (int i) { return 2*i+2; }
+    void insertion(int num){
+        if(capacity == size) return;
+        ++size;
+        int i = size-1;
+        arr[i] = num;
+
+        int par = getparent(i);
+        while(i != 0 and arr[par] < arr[i]){
+            swap(arr[par], arr[i]), i = par;
+            if (i == 0) break;
             par = getparent(i);
         }
     }
-    void heapify(int i){
-        int largest = i;
-        int l = getleft(i), r = getright(i);
-        if(l < heapsize && arr[largest] < arr[l]) largest = l;
-        if(r < heapsize && arr[r] > arr[largest]) largest = r;
-        if(largest == i) return;
-        swap(arr[largest], arr[i]);
-        heapify(largest);
-    }
     int extractmax(){
-        if(heapsize==0){
-            cout<<"Undeflow!"<<endl;
-            return -1;
-        }
-        int ans = arr.front();
-        --heapsize;
-        swap(arr[0], arr[heapsize]);
-        heapify(0);
-        return ans;
+        int save = arr[0];
+        swap(arr[0], arr[size-1]);
+        --size;
+        heapify(0); 
+        return save;
+    }
+    void heapify(int i){
+        int big = i;
+        int left = ileft(i), right = iright(i);
+        if(left < size && arr[big] < arr[left]) big = left;
+        if(right < size && arr[big] < arr[right]) big = right;
+        if(big == i) return ;
+        swap(arr[big], arr[i]);
+        heapify(big);
+    }
+    void printing(){
+        for(int i = 0; i<size; i++) cout<<arr[i]<<" ";
+        cout<<endl;
     }
     void heapsort(){
-        for(int i =0 ;i <= heapsize+1; i++){
+        while(size){
             cout<<extractmax()<<" ";
         }
     }
@@ -65,6 +63,7 @@ int main(){
     maxheap->insertion(11);
     maxheap->insertion(15);
     maxheap->insertion(21);
+    maxheap->printing();
     maxheap->heapsort();
     return 0;
 }
--- minbinaryheap.cpp
+++ minbinaryheap.cpp
@@ -1,87 +1,65 @@
-/*
-    minheap(int cap);
-    
-    int parent(int i ){ return (i-1)/2;}
-
-    int left(int i){ return 2*i+1; }
-    
-    int right(int i ) { return 2*i+2; }
-    
-    int getmin() { return arr[0]; }
-    
-    void inserting(int key);
-    
-    void extractmin();
-    
-    void heapify(int i);
-    
-    void printing();
-    
-    void heapsort();
-*/
 #include<bits/stdc++.h>
 
 using namespace std;
 
 class MinHeap{
-    int capacity;
-    int heapsize;
-    vector<int> arr;
-    public:
-    MinHeap(int cap){
-        capacity = cap;
-        arr= vector<int>(cap, INT_MAX);
-        heapsize = 0;
-    }
-    int fparent(int i) { return (i-1)/2; }
-    int fleft(int i) { return 2*i + 1; }
-    int fright(int i) { return 2*i + 2; }
-    void insertion(int val){
-        if(heapsize == capacity){
-            cout<<"Overflow!"<<endl;
-            return;
-        }
+  public:
+  vector<int> arr;
+  int capacity, size;
+  MinHeap(int num){
+      capacity = num, size = 0;
+      arr = vector<int> (num);
+  }
+  int iparent(int i) { return (i-1)/2; }
+  int ileft(int i) { return 2*i + 1; }
+  int iright(int i) { return 2*i + 2; }
+  void insertion(int num){
+    if(capacity == size) return;
 
-        ++heapsize;
-        arr[heapsize-1] = val;
-        int i = heapsize-1;
-        int par = fparent(i);
-        while(i > 0 && arr[par] > arr[i]){
-            swap(arr[i], arr[par]);
-            i = par;
-            par = fparent(i);
-        }
+    ++size;
+    arr[size-1] = num;
+    insertionsupport(size-1);
+  }
+  void insertionsupport(int i){
+    int par = iparent(i);
+    if(arr[par] > arr[i]){
+      swap(arr[par], arr[i]);
+      if(par != 0)
+        insertionsupport(par);
     }
-    int extractmin(){
-        if(heapsize == 0){
-            cout<<"Underflow!"<<endl;
-            return -1;
-        }
-        int ans = arr.front();
-        --heapsize;
-        swap(arr[0], arr[heapsize]);
-        heapify(0);
-        return ans;
+  }
+  int front() { return arr[0]; }
+  int extractmin(){
+    int minimum_element = arr[0];
+    swap(arr[0], arr[size-1]);
+    --size;
+    heapify(0);
+    return minimum_element;
+  }
+  void heapify(int i){
+    int small = i;
+    int l = ileft(i), r = iright(i);
+    if(l < size && arr[small] > arr[l])
+      small = l;
+    if(r < size && arr[small] > arr[r])
+      small = r;
+    
+    if(small == i) return;
+    swap(arr[small], arr[i]);
+    heapify(small);
+    return;
+  }
+  void heapsort(){
+    while(size){
+      cout<<extractmin()<<" ";
     }
-    void heapify(int i){
-        int l = fleft(i);
-        int r = fright(i);
-        int smallest = i;
-        
-        if(l <heapsize && arr[smallest] > arr[l]) smallest = l;
-        if(r < heapsize && arr[smallest] > arr[r]) smallest = r;
-        
-        if(smallest != i){
-        swap(arr[i], arr[smallest]);
-        heapify(smallest);
-        }
-    }
-    void heapsort(){
-        cout<<"heapsize is "<<heapsize<<endl;
-        for(int i = 0; i<heapsize; i++){
-            cout<<extractmin()<<" ";
-        }
-    }
+    cout<<endl;
+  }
+  void printing(){
+    for(int i =0; i<size; i++)
+      cout<<arr[i]<<" ";
+    cout<<endl;
+  }
 };
 
 int main(){
@@ -94,7 +72,10 @@ int main(){
     minheap->insertion(11);
     minheap->insertion(15);
     minheap->insertion(21);
+    cout<<minheap->front()<<endl;
+    minheap->printing();
     minheap->heapsort();
+    
     // MinHeap minheap(10);
     // minheap.insertion(20);
     // minheap.insertion(3);
@@ -108,93 +89,3 @@ int main(){
     delete minheap;
     return 0;
 }
-/*
-Working code...
-#include<bits/stdc++.h>
-
-using namespace std;
-
-class minheap{
-    public:
-  vector<int> arr;
-  int capacity;
-  int heapsize;
-  
-  minheap(int limit){
-    capacity = limit;
-    heapsize = 0;
-    arr = vector<int>(limit);
-  }
-  
-  int fparent(int i){ return (i-1)/2; }
-  int fleft(int i ){ return 2*i+1; }
-  int fright(int i){ return 2*i+2; }
-  
-  void inserting(int elem){
-    if(heapsize == capacity){
-      cout<<"Overflow!"<<endl;
-      return;
-    }
-    
-    ++heapsize;
-    arr[heapsize-1] = elem;
-    int i = heapsize-1;
-    int par = fparent(i);
-    while(i>0 && arr[i]<arr[par]){
-      swap(arr[i], arr[par]);
-      i = par;
-      par = fparent(i);
-    }
-  }
-  
-  int extractmin(){
-    if(heapsize == 0){
-      cout<<"Underflow!";
-      return -1;
-    }
-    int minelem = arr.front();
-    --heapsize;
-    swap(arr[0], arr[heapsize]);
-    
-    heapify(0);
-    return minelem;
-  }
-  
-  void heapify(int i){
-    int l = fleft(i);
-    int r = fright(i);
-    int smallest = i;
-    
-    if(l <heapsize && arr[smallest] > arr[l]) smallest = l;
-    if(r < heapsize && arr[smallest] > arr[r]) smallest = r;
-    
-    if(smallest != i){
-      swap(arr[i], arr[smallest]);
-      heapify(smallest);
-    }
-  }
-  
-  void heapsort(){
-    int len = heapsize;
-    for(int i = 0; i< len; i++){
-      cout<<extractmin()<<" ";
-    }
-  }
-};
-
-int main(){
-    minheap h1(11);
-    h1.inserting(20);
-    h1.inserting(3);
-    h1.inserting(10);
-    h1.inserting(9);
-    h1.inserting(7);
-    h1.inserting(11);
-    h1.inserting(15);
-    h1.inserting(21);
-    //h1.inserting(90);
-    //h1.inserting(100);
-    //h1.extractmin();
-    //h1.printing();
-    h1.heapsort();
-}*/
