c3da22f804d386c71d824832efdf7cef4e2b0670
Sincere-Dhairya <varshneydhairya4@gmail.com>
2022-06-03 23:38:55 +0530

    Prefix Sum and DP

--- leetcode/304. Range Sum Query 2D - Immutable.cpp
+++ leetcode/304. Range Sum Query 2D - Immutable.cpp
@@ -73,3 +73,23 @@ public:
 
 // Dynamic Programming.
 // O(1).
+class NumMatrix {
+public:
+    vector<vector<long>> table;
+    int n=0, m=0;
+    NumMatrix(vector<vector<int>>& matrix) {
+        n = matrix.size()+1, m = matrix[0].size()+1;
+        table = vector<vector<long>>(n, vector<long>(m));
+        for(int i=1; i<n; ++i){
+            for(int j=1; j<m; ++j){
+                table[i][j] = matrix[i-1][j-1]+table[i-1][j]+table[i][j-1]-table[i-1][j-1];
+            }
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        int area1 = table[row2+1][col2+1], area2 = table[row1][col2+1], 
+        area3 = table[row2+1][col1], area4= table[row1][col1];
+        return area1 - area2 - area3 + area4;
+    }
+};
